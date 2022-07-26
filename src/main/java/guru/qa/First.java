b49931320baedf203a54cd0c5ed4c34b8c4fd7b1
taniytanya <taniytanya@gmail.com>
2022-07-26 22:49:20 +0300

    Выгрузка задания с урока

---
+++ src/main/java/guru/qa/First.java
@@ -0,0 +1,10 @@
+package guru.qa;
+
+public class First {
+    public static void main(String[] args) {
+        Person teacher = new Person(33,"Dima");
+        Person teacher1 = new Person();
+        teacher.goToPension();
+        teacher.sayName();
+    }
+}
